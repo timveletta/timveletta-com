699440eceb20cccf1fb064cb6acad6d36edad42c
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-01-28 00:09:45 +0800

    feat: adding blog post content to rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,21 @@
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
+export const get = () => {
+	const postImportResult = import.meta.glob('./blog/**/*.{md,mdx}', {
+		eager: true,
+	});
+	const posts = Object.values(postImportResult);
+
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: posts.map((post) => ({
+			link: post.url,
+			content: sanitizeHtml(post.compiledContent()),
+			...post.frontmatter,
+		})),
 	});
+};
