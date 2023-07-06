30c26d6cb9e5454851121c2d16b0c9ff2f8ac5db
Tim Veletta <timothy.veletta@gmail.com>
2023-07-06 17:12:51 +0800

    moving emailtemplate

--- api/contact.js
+++ api/contact.js
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-import EmailTemplate from '../src/components/EmailTemplate';
+import EmailTemplate from './EmailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
