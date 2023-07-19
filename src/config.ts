5a3b7cd98485fa71f14644c7b2e9776804626bf8
Tim Veletta <timothy.veletta@gmail.com>
2023-07-19 16:36:58 +0800

    update description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tim Veletta';
-export const SITE_DESCRIPTION = 'Tim Veletta';
+export const SITE_DESCRIPTION =
+	'The personal portfolio and blog of me, Tim Veletta.';
