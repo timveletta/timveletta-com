e005fe253890197680b9bbf5f966447a394cefec
Tim Veletta <timothy.veletta@gmail.com>
2023-09-06 10:08:35 +0800

    upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,12 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 import vercel from '@astrojs/vercel/static';
 
-// https://astro.build/config
-import image from '@astrojs/image';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://timveletta.com',
-	integrations: [
-		react(),
-		mdx(),
-		sitemap(),
-		tailwind(),
-		image({ serviceEntryPoint: '@astrojs/image/sharp' }),
-	],
+	integrations: [react(), mdx(), sitemap(), tailwind()],
 	output: 'static',
 	adapter: vercel(),
 });
