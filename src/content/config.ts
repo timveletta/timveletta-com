4606eba6f4b1606544f7e57f6e2cb6207c2f48c8
Tim Veletta <480823+timveletta@users.noreply.github.com>
2023-11-08 14:10:28 +0800

    adding TIL pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,32 +1,42 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		pubDate: z.date(),
-		draft: z.boolean().optional(),
-		description: z.string(),
-		tags: z.array(z.string()).optional(),
-		heroImage: z.string(),
-		imageCreditName: z.string(),
-		imageCreditLink: z.string(),
-		prevUrl: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    draft: z.boolean().optional(),
+    description: z.string(),
+    tags: z.array(z.string()).optional(),
+    heroImage: z.string(),
+    imageCreditName: z.string(),
+    imageCreditLink: z.string(),
+    prevUrl: z.string().optional(),
+  }),
 });
 
 const projectCollection = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		heroImage: z.string(),
-		tags: z.array(z.string()),
-		githubLink: z.string().optional(),
-		websiteLink: z.string().optional(),
-		featured: z.boolean().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    heroImage: z.string(),
+    tags: z.array(z.string()),
+    githubLink: z.string().optional(),
+    websiteLink: z.string().optional(),
+    featured: z.boolean().optional(),
+  }),
+});
+
+const tilCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    updatedAt: z.date(),
+    draft: z.boolean().optional(),
+    category: z.string(),
+  }),
 });
 
 export const collections = {
-	blog: blogCollection,
-	projects: projectCollection,
+  blog: blogCollection,
+  projects: projectCollection,
+  til: tilCollection,
 };
---
+++ src/content/til/test-learning.md
@@ -0,0 +1,8 @@
+---
+title: This is a test learning
+updatedAt: 2022-01-28
+draft: false
+category: Test
+---
+
+This is some test learning.
