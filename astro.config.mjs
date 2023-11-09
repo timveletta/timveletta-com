37e46694b092e8a3ef12bae6f93162ff08d78c1e
Tim Veletta <480823+timveletta@users.noreply.github.com>
2023-11-09 12:13:53 +0800

    small fixes to til and uses pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,31 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+const tina = ({ directiveName = "tina" } = {}) => ({
+  name: "tina-cms",
+  hooks: {
+    "astro:config:setup": ({ addClientDirective, opts }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: "./tina/tina.mjs",
+      });
+    },
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://timveletta.com',
-	integrations: [react(), mdx(), sitemap(), tailwind()],
-	output: 'static',
-	adapter: vercel(),
+  site: "https://timveletta.com",
+  integrations: [react(), mdx(), sitemap(), tailwind(), tina()],
+  output: "static",
+  adapter: vercel(),
 });
