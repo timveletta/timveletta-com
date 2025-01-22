cc841a2e129018d75a41db3461aaabee57bce823
Tim Veletta <480823+timveletta@users.noreply.github.com>
2025-01-22 23:46:00 +0800

    Changing container width

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,41 +1,41 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		container: {
-			screens: {
-				'2xl': '1280px',
-			},
-		},
-		extend: {
-			maxHeight: {
-				'1/2': '50vh',
-			},
-			height: {
-				hero: 'calc(100vh - 6rem)',
-				250: '250px',
-				200: '200px',
-			},
-			maxWidth: {
-				'1/2': '50vw',
-			},
-			colors: {
-				primary: '#01bfa6',
-				secondary: '#0f3d56',
-				grey: '#F4F4F4',
-			},
-			inset: {
-				'1/2': '50%',
-			},
-			gridTemplateColumns: {
-				contact: '1fr 2fr',
-			},
-			fontFamily: {
-				sans: ['Mulish', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      screens: {
+        xl: "1024px",
+      },
+    },
+    extend: {
+      maxHeight: {
+        "1/2": "50vh",
+      },
+      height: {
+        hero: "calc(100vh - 6rem)",
+        250: "250px",
+        200: "200px",
+      },
+      maxWidth: {
+        "1/2": "50vw",
+      },
+      colors: {
+        primary: "#01bfa6",
+        secondary: "#0f3d56",
+        grey: "#F4F4F4",
+      },
+      inset: {
+        "1/2": "50%",
+      },
+      gridTemplateColumns: {
+        contact: "1fr 2fr",
+      },
+      fontFamily: {
+        sans: ["Mulish", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
