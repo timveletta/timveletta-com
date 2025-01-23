1328fc8183ee499c3efafd776268ecec37cef7a4
Tim Veletta <480823+timveletta@users.noreply.github.com>
2025-01-23 10:41:05 +0800

    Remove vercel adapter since its a static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://timveletta.com",
   integrations: [react(), mdx(), sitemap(), tailwind()],
   output: "static",
-  adapter: vercel(),
 });
